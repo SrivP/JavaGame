fae92c72021f72ea9007c3d60ba3228e636a1d9b
Sriv P <SrivPrasanna@gmail.com>
2022-12-02 20:15:45 +0000

    menu img bg update

--- Meny.java
+++ Meny.java
@@ -6,11 +6,21 @@ import java.util.EventListener;
 
 
 public class Fight_1 extends JFrame implements EventListener {
-    ImageIcon imageIcon = new ImageIcon("");
+    private Container c;
+}
+
+class menu extends JPanel {
+    ImageIcon imageIcon = new ImageIcon("/workspaces/JavaGame/cq52pcj3ga491.gif");
+
+    public menu() {
+        
+    }
 
 
-
-
+    public void paintComponent(Graphics g) {
+        super.printComponent(g);
+        g.drawImage(imageIcon.getImage(), 0, 0, 300, 300, null);
+    }
 
 
 
